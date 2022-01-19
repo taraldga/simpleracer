1abd7c6910afd0828368e17c98e2d1b4bf680944
Tarald <taraldga@gmail.com>
2022-01-19 12:35:45 +0100

    fix: remove library that creates deadlock on first migration

--- backend/races/serializers.py
+++ backend/races/serializers.py
@@ -1,39 +1,31 @@
 from rest_framework import serializers
-from .models import Race, Participant, Result
-from django_typomatic import ts_interface, generate_ts
+from races import models
 
-@ts_interface()
 class ParticipantSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Participant
+        model = models.Participant
         fields = ['id', 'name', 'race', 'email', 'start_nr']
         read_only_fields = ['start_nr']
 
 
-@ts_interface()
 class ResultSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Result
+        model = models.Result
         fields = "__all__"
 
-
-@ts_interface()
 class RaceSerializer(serializers.ModelSerializer):
     # participants = ParticipantSerializer(source='participant_set')
 
     class Meta:
-        model = Race
+        model = models.Race
         fields = ['id', 'name', 'description', 'start_time', 'participant_set']
         depth = 1
 
 
-@ts_interface()
 class RaceListSerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Race
+        model = models.Race
         fields = ['id', 'name', 'description', 'start_time']
         depth = 1
 
-
-generate_ts('../frontend/models/Race.ts')
